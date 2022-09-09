633ef36f26ff6e8d20ea14841e09e04407a0d24c
fowowe Oluwatimilehin <t.fowowe123@gmail.com>
2022-09-09 16:18:38 -0700

    tebah clear

--- 0x01-variables_if_else_while/7-print_tebalpha.c
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -1,1 +1,16 @@
-#include <stdio>
+#include <stdio.h>
+/**
+ *
+ * main - entry point
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z' ; a >= 'a' ; a--)
+		putchar('\n');
+	return (0);
+}
