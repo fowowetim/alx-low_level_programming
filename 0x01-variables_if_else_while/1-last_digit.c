c9189c95b8e2f715ef8b8d48a7b1e8f6754ed322
fowowe Oluwatimilehin <t.fowowe123@gmail.com>
2022-09-09 13:53:39 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,20 +14,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastno = n & 10;
-	if(lastno > 5)
+	lastno = n % 10;
+	if (lastno > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastno);
-	}
-	else if(lastno == 0)
+	}else if (lastno == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastno);
-	}
-	else
+	}else
 	{	
 		printf("Last digit of %d is %d and less than 6 and not 0\n", n, lastno);
 	}
-
 	return (0);
 }
 
