ae24dbad483aab311b5b6aefda423996bd59204d
fowowe Oluwatimilehin <t.fowowe123@gmail.com>
2022-09-09 18:57:06 -0700

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,11 +18,11 @@ int main(void)
 			for (k = 0; k <= 9; j++)
 			{
 				if (k > j && j > i)
-				{ 
+				{
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-					if (i != 7 || j !=8 || k !=9)
+					if (i != 7 || j != 8 || k != 9)
 					{
 						putchar(',');
 						putchar(' ');
