9f79aed812c1c9c7a4820bbe5716940cb8b43f02
fowowe Oluwatimilehin <t.fowowe123@gmail.com>
2022-09-13 14:52:01 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+ * print_alphabet - function to print abc
  *
  * Return: 0
  */
@@ -7,7 +8,7 @@ void print_alphabet(void)
 {
 	char c;
 
-	for(c = 'a'; c <=  'z'; c++)
+	for (c = 'a'; c <=  'z'; c++)
 	{
 		_putchar(c);
 	}
