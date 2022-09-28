1187e764a6cf450167e8ab5e0a0113dbc680ebe8
fowowe Oluwatimilehin <t.fowowe123@gmail.com>
2022-09-28 16:18:05 -0700

    task 6 solved

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -19,20 +19,20 @@ int evaluate_num(int num, int iterator)
 	if (num % iterator != 0)
 	{
 		return (evaluate_num(num, iterator + 1));
-		return (0);
 	}
+	return (0);
 }
 
 /**
-* is_prime_number - evaluate prime or not
-* @num: number.
-* Return: return 1 prime - return 0 otherwise
-*/
+ * is_prime_number - evaluate prime or not
+ * @num: number
+ * Return: return 1 prime - return 0 otherwise
+ */
 
 int is_prime_number(int num)
 {
 	int iterator;
-		
+
 	iterator = 2;
 	if (num < 2)
 	{
